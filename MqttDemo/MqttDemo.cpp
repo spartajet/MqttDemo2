71fcca853d98fdad866b74a0e133e036605b0a68
guoxiaozhong <guoxiaozhong1990@outlook.com>
2019-10-17 19:23:03 +0800

    配置完成

--- MqttDemo/MqttDemo.cpp
+++ MqttDemo/MqttDemo.cpp
@@ -8,6 +8,12 @@ MqttDemo::MqttDemo(QWidget *parent)
 
 void MqttDemo::on_ConnectControlButton_clicked()
 {
+	QString ip = this->ui.IpEdit->text();
+	QString port = this->ui.PortEdit->text();
+	this->IpAdress = ip.append(port).toStdString();
+	this->ClientId = this->ui.ClientIdEdit->text().toStdString();
+	this->async_client_ = new async_client(this->IpAdress,this->ClientId);
+	
 }
 
 void MqttDemo::on_SubscribeButton_clicked()
